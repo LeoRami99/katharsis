6ad6a6dd6f7b0ba70d3a8abb4c2118a6441087de
Leonardo Ramirez <juanlov4321@gmail.com>
2024-11-13 18:29:21 -0500

    fix: correct capitalization in section title for consistency

--- src/app/components/Sections/SectionMatriz.tsx
+++ src/app/components/Sections/SectionMatriz.tsx
@@ -1,7 +1,7 @@
 const SectionMatriz = () => {
     return (
         <section className="py-20 bg-white" id="matriz">
-            <h1 className="text-black text-6xl font-bold text-center mt-10 mb-16">Matriz De Evaluación De Ideas</h1>
+            <h1 className="text-black text-6xl font-bold text-center mt-10 mb-16">Matriz de Evaluación de Ideas</h1>
             <div className="flex flex-col justify-center items-center">
                 <img src="/rocket.png" alt="Matriz Rocket" width={800} height={800} className="mx-auto border rounded-xl" />
             </div>
